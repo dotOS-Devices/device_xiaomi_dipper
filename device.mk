d3879ca07193f55f115142cb502630770bd8073c
Paul Crowley <paulcrowley@google.com>
2019-04-01 13:44:46 -0700

    dipper: Adoptable storage: Use CTS filenames encryption, not HEH
    Test: adb shell sm set-virtual-disk true
    Bug: 123297412
    Change-Id: Ide44f4e59be85e081b8e12c3e1d86b4a830dda82
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- device.mk
+++ device.mk
@@ -80,6 +80,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/gps_debug.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/gps_debug.conf
 
+# HEH filename encryption is being dropped
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.crypto.volume.filenames_mode=aes-256-cts
+
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/idc/uinput-fpc.idc:$(TARGET_COPY_OUT_SYSTEM)/usr/idc/uinput-fpc.idc \
