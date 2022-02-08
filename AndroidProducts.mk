a3e537c8eba73d20cca21820ceeb5a81f16ac7a4
KyuoFoxHuyu <88403766@qq.com>
2022-02-08 14:51:42 +0800

    dipper: Bring up DotOS
    Signed-off-by: KyuoFoxHuyu <88403766@qq.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_dipper.mk
+    $(LOCAL_DIR)/dot_dipper.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_dipper-user \
-    lineage_dipper-userdebug \
-    lineage_dipper-eng
+    dot_dipper-user \
+    dot_dipper-userdebug \
+    dot_dipper-eng
