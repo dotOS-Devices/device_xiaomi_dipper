934248527b304a398a4ecb525fbcd91a7c76f581
KyuoFoxHuyu <88403766@qq.com>
2022-02-08 14:55:35 +0800

    dipper: set TARGET_BOOT_ANIMATION_RES to 1080
    Signed-off-by: KyuoFoxHuyu <88403766@qq.com>

--- dot_dipper.mk
+++ dot_dipper.mk
@@ -8,6 +8,7 @@ $(call inherit-product, device/xiaomi/dipper/device.mk)
 
 # Inherit some common DotOS stuff.
 $(call inherit-product, vendor/dot/config/common.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := dot_dipper
