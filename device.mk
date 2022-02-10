1464e404439fdcf94a1057e31e8cab17b8f0c90f
KyuoFoxHuyu <88403766@qq.com>
2022-02-11 00:43:46 +0800

    dipper: Drop firmware include
    * Now, we switch back to keymaster3.0
    
    Signed-off-by: KyuoFoxHuyu <88403766@qq.com>

--- device.mk
+++ device.mk
@@ -15,9 +15,6 @@ OVERRIDE_PRODUCT_COMPRESSED_APEX := false
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/xiaomi/dipper/dipper-vendor.mk)
 
-# Firmware
-$(call inherit-product, vendor/xiaomi/dipper-firmware/firmware.mk)
-
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2248
 TARGET_SCREEN_WIDTH := 1080
